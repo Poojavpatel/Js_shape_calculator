6580f98d45b3e5a733d2fab03bd7409e45ea92bd
Pooja Patel <poojavpatel71@gmail.com>
2019-06-30 02:45:30 -0700

    screen 1 in progress

---
+++ README.md
@@ -0,0 +1,1 @@
+# JS Shape Calculator
---
+++ script.js
@@ -0,0 +1,13 @@
+function getShape(){
+    // if nothing selected alert select one
+    const shape = document.getElementById("shapes").elements["shape"].value;
+    console.log(shape);
+}
+
+function inputDimension(){
+
+}
+
+function displayArea(area){
+
+}
